8c1baedbc145c2ff8d229bd1b0f4d4fb843a31a1
jidibinlin <2694273649@qq.com>
2025-03-12 02:22:56 +0800

    feat: remove comment lines from early-init.el

--- early-init.el
+++ early-init.el
@@ -1,18 +1,5 @@
-;;; early-init.el --- Description -*- lexical-binding: t; -*-
-;;
-;; Copyright (C) 2024 jidibinlin
-;;
-;; Author: jidibinlin <2694273649@qq.com>
-;; Maintainer: jidibinlin <2694273649@qq.com>
-;; Created: November 01, 2024
-;; Modified: November 01, 2024
-;; Version: 0.0.1
-;; Keywords: Symbol’s value as variable is void: finder-known-keywords
-;; Homepage: https://github.com/qibin/early-init
-;; Package-Requires: ((emacs "24.3"))
-;;
-;; This file is not part of GNU Emacs.
-;;
+;;; early-init.el --- emacs early init file -*- lexical-binding: t; -*-
+
 ;;; Commentary:
 ;;
 ;;  Description
